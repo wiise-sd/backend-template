c3ff57aaf11a7e182abc50e4787bea1924fdd52c
Carlos Sandro <sandro.dl17@hotmail.com>
2025-03-13 14:42:04 -0300

    chore: add eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+   { ignores: ["node_modules", "dist"] },
+   { files: ["**/*.{js,mjs,cjs,ts}"] },
+   { languageOptions: { globals: globals.node } },
+   pluginJs.configs.recommended,
+   ...tseslint.configs.recommended,
+   {
+      rules: {
+         "@typescript-eslint/no-explicit-any": "off",
+      },
+   },
+];
